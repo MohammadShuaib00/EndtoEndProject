0b4bbf9d47b3434cce8f0ff1ff74bf02e8b4737c
Mohammad Shuaib <mohammadshuaib3455@gmail.com>
2024-08-13 10:32:51 +0530

    Data Ingestion Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,18 +12,42 @@ from pathlib import Path
 
 @dataclass
 class DataIngestionConfig:
-    pass
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
 
 class DataIngestion:
     def __init__(self):
-        pass
+        self.ingestion_config=DataIngestionConfig()
     
     def initate_data_ingestion(self):
+        logging.info("Data Ingestion Started..")
         try:
-            pass
+            data = pd.read_csv("src/components/gemstone.csv")
+            logging.info("Data Successfully Loaded")
+            
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" I have saved the raw dataset in artifact folder")
+            
+            logging.info("Here I have performed train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("Data ingestion part completed")
+            
         except Exception as e:
             logging.info()
             raise CustomException(e,sys)
     
+    
+if __name__=="__main__":
+    obj=DataIngestion()
+
+    obj.initate_data_ingestion()
     
 
